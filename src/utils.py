a0d0eed8f361ce5171154c878e73324b3d3a40ef
UmangMudgal <mudgal0709@gmail.com>
2023-04-13 21:52:06 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -21,3 +21,27 @@ def save_object(file_path, obj):
     except Exception as e:
         logging.info('Exception Occurred in save object function')
         raise CustomException(e, sys)
+    
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+            # Train model
+            model.fit(X_train,y_train)
+
+            
+
+            # Predict Testing data
+            y_test_pred =model.predict(X_test)
+
+            # Get R2 scores for train and test data
+            
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] =  test_model_score
+
+        return report
+    except Exception as e:
+        logging.info('Exception Occurred during Evalution of Model')
+        raise CustomException(e, sys)
